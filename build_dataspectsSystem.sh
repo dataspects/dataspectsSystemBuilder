8e1ce39c651dfb577e330b0f2cfe297e78911b76
Lex Sulzer <lex@sulzer.net>
2018-11-05 14:56:44 +0100

    Pruned of all dataspects SEARCH/ONTOLOGY stuff

--- build_dataspectsSystem.sh
+++ build_dataspectsSystem.sh
@@ -30,11 +30,4 @@ time ansible-playbook \
       ansible_playbooks/install_mediawiki.yml \
       ansible_playbooks/install_bluespice_pro.yml \
       ansible_playbooks/install_mediawiki_extensions.yml \
-      ansible_playbooks/place_run_containers_manually_scripts.yml \
-      ansible_playbooks/place_system_profiles.yml \
-      ansible_playbooks/inject_dataspectsSystemCoreOntology_into_mediawiki.yml \
-      ansible_playbooks/execute_mediawiki_maintenance_runJobs.yml \
-      ansible_playbooks/reset_elasticsearch_index.yml \
-      ansible_playbooks/place_dataspects_search_config_files.yml \
-    # ../ProvisionDSAsDSCookbook/ansible_playbooks/index_dataspectsStandardSystem.yml \
-    # ../ProvisionDSAsDSCookbook/ansible_playbooks/index_dataspectsSystemSoftware.yml
+      ansible_playbooks/execute_mediawiki_maintenance_runJobs.yml
