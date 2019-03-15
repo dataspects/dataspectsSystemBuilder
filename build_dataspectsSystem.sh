af3322d318ad7e622b589d9085b93fc198b65c9f
Lex Sulzer <lex@sulzer.net>
2019-03-15 17:55:02 +0100

    Added feeder logic

--- build_dataspectsSystem.sh
+++ build_dataspectsSystem.sh
@@ -9,29 +9,32 @@ if [[ ! $AWS_SECRET_ACCESS_KEY ]]; then AWS_SECRET_ACCESS_KEY=dummy; fi
 if [[ ! $UI_SESSION_SECRET ]]; then UI_SESSION_SECRET=dummy; fi
 
 ANSIBLETAGS=(
-  ### 010_Prepare
-    create_dataspectsSystem_control_folder_on_host
-    pull_private_Docker_images_from_registry_dataspects_com
-  ### 020_Docker-Compose
-    compile_and_copy_docker_compose_file
-    run_docker_compose
-  ### Comment all following tags for manual installation on Docker stack in accordance with C1470408196
-  ### 030_MediaWiki
-    install_mediawiki
-    configure_proxy
-    install_mediawiki_extensions
-    execute_mediawiki_maintenance_runJobs
-  ### 040_Ontologies
-    provision_as_cookbookfalnet
-  ## 050_Indexing
+  # ### 010_Prepare
+  #   create_dataspectsSystem_control_folder_on_host
+  #   pull_private_Docker_images_from_registry_dataspects_com
+  # ### 020_Docker-Compose
+  #   compile_and_copy_docker_compose_file
+  #   run_docker_compose
+  # ### Comment all following tags for manual installation on Docker stack in accordance with C1470408196
+  # ### 030_MediaWiki
+  #   install_mediawiki
+  #   configure_proxy
+  #   install_mediawiki_extensions
+  #   execute_mediawiki_maintenance_runJobs
+  # ### 040_Ontologies
+  #   provision_as_cookbookfalnet
+  ### 045 Feeding
+    feed_cookbook_entities
+  #  feed_dataspectsSystem_source_folder
+  # ### 050_Indexing
     index_cookbook_entities
-    index_dataspectsSystem_source_folder
-  ### 060_Backup_and_Clone
-    install_backup_functionality
-    install_clone_functionality
-    backup_and_clone
-  ### 070_Tools
-    compare
+  #   index_dataspectsSystem_source_folder
+  # ### 060_Backup_and_Clone
+  #   install_backup_functionality
+  #   install_clone_functionality
+  #   backup_and_clone
+  # ### 070_Tools
+  #   compare
 
 )
 
@@ -55,6 +58,8 @@ time ansible-playbook \
       ansible_playbooks/030_MediaWiki/install_mediawiki_extensions.yml \
       ansible_playbooks/030_MediaWiki/execute_mediawiki_maintenance_runJobs.yml \
       ansible_playbooks/040_Ontologies/provision_as_cookbookfalnet.yml \
+      ansible_playbooks/045_Feeding_Raw_Data/feed_cookbook_entities.yml \
+      ansible_playbooks/045_Feeding_Raw_Data/feed_dataspectsSystem_source_folder.yml \
       ansible_playbooks/050_Indexing/index_cookbook_entities.yml \
       ansible_playbooks/050_Indexing/index_dataspectsSystem_source_folder.yml \
       ansible_playbooks/060_Backup_and_Clone/install_backup_and_clone_functionality.yml \
