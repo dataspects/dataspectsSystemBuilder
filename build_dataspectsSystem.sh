9aa0b0fd07ec95c5af9adc5bea78702d43ac5e44
Lex Sulzer <lex@sulzer.net>
2019-04-08 17:03:21 +0200

    Added DataspectsMediaWikiFeeder

--- build_dataspectsSystem.sh
+++ build_dataspectsSystem.sh
@@ -22,14 +22,14 @@ ANSIBLETAGS=(
   310_configure_proxy
   320_install_mediawiki_extensions
   330_execute_mediawiki_maintenance_runJobs
-  # 400_provision_as_cookbookfalnet
-  # ### For the time being this requires manually running dataspects-ui
-  # 500_configure_nodejs
-  # 600_feed_cookbook_entities
-  # 610_feed_dataspectsSystem_source_folder
-  # 700_prepare_indexing
-  # 710_index_cookbook_entities
-  # 720_index_dataspectsSystem_instance_source_folder
+  400_provision_as_cookbookfalnet
+  ### For the time being this requires manually running dataspects-ui
+  500_configure_nodejs
+  600_feed_cookbook_entities
+  610_feed_dataspectsSystem_source_folder
+  700_prepare_indexing
+  710_index_cookbook_entities
+  720_index_dataspectsSystem_instance_source_folder
   # 800_install_backup_functionality
   # 800_install_clone_functionality
   # 810_backup_and_clone
