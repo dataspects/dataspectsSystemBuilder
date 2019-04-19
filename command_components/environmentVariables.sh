bf2e91201d4651d2b18353fa555e71f8db292b3c
Lex Sulzer <lex@sulzer.net>
2019-04-19 16:28:04 +0200

    Renamed to ...full-system

--- command_components/environmentVariables.sh
+++ command_components/environmentVariables.sh
@@ -6,6 +6,6 @@ if [[ ! $TIKA_USERNAME ]]; then TIKA_USERNAME=dummy; fi
 if [[ ! $TIKA_PASSWORD ]]; then TIKA_PASSWORD=dummy; fi
 if [[ ! $UI_FEED_SERVICE_API_KEY ]]; then UI_FEED_SERVICE_API_KEY=aslkdjasldkjlaskdj; fi
 if [[ ! $DOCKER_VOLUMES_MODE ]]; then DOCKER_VOLUMES_MODE=automatic; fi
-if [[ ! $DOCKER_COMPOSE_FILE ]]; then DOCKER_COMPOSE_FILE=docker-compose-standard; fi
+if [[ ! $DOCKER_COMPOSE_FILE ]]; then DOCKER_COMPOSE_FILE=docker-compose-full-system; fi
 if [[ ! $PLATFORM ]]; then PLATFORM="linux"; fi
 if [ $PLATFORM == "linux" ]; then PRIVILEGE_ESCALATION="--ask-become-pass --become-method sudo"; else PRIVILEGE_ESCALATION=""; fi
