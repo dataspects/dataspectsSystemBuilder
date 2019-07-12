fc43065260235234c269778d0c606085bb3c24a8
Lex Sulzer <lex@sulzer.net>
2019-07-12 10:03:04 +0200

    Added error code

--- ansible_templates/install_composer.sh
+++ ansible_templates/install_composer.sh
@@ -6,6 +6,7 @@ ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
+    # Error 1234:56 PHP Composer invalid installer signature
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
     exit 1
