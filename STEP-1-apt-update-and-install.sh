b703259d8d079386f954aef3b1ab53a0a7fb8a4c
Lex Sulzer <lex@dataspects.com>
2020-07-10 11:44:15 +0200

    Working mediawiki restore

--- STEP-1-apt-update-and-install.sh
+++ STEP-1-apt-update-and-install.sh
@@ -2,5 +2,7 @@
 
 ssh -p 2222 ds@localhost
 
-# ds@dataspects:~$ sudo apt update && sudo apt upgrade && sudo apt install git docker-compose curl ruby lynx restic net-tools sshfs wget virtualbox-guest-utils virtualbox-guest-additions-iso
+# ds@dataspects:~$ sudo apt update && sudo apt upgrade && sudo apt install git docker-compose curl ruby lynx restic net-tools sshfs wget
+
+# ds@dataspects:~$ sudo apt install virtualbox-guest-utils virtualbox-guest-additions-iso
 # ds@dataspects:~$ sudo mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /media/ && sudo /media/VBoxLinuxAdditions.run
