5d7c458fb004a1f726d5530d7f8448438789118b
Lex Sulzer <lex@dataspects.com>
2021-01-23 11:51:34 +0100

    Added restore script

--- build-distribution-package.sh
+++ build-distribution-package.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-BASE_MEDIAWIKI_ROOT_FOLDER=/var/lib/docker/volumes/dataspectssearch_mediawiki_root/_data/w
-MEDIAWIKI_SUNFLOWER_DISTRIBUTION_FOLDER=/media/lex/LEXSAMSUNG-64GB/mediawiki-sunflower-distribution
+BASE_MEDIAWIKI_ROOT_FOLDER=/home/dserver/mediawiki_root/w
+MEDIAWIKI_CANASTA_DISTRIBUTION_FOLDER=/home/dserver
 
-APACHE_CONTAINER_NAME=dataspectssearch-mediawikiservice
+APACHE_CONTAINER_NAME=mediawiki
 
 MYSQL_HOST=127.0.0.1
-DATABASE_NAME=wikidb
+DATABASE_NAME=mediawiki
 MYSQL_USER=mediawiki
 MYSQL_USER_PASSWORD=mediawikipass
 
 # Dump database
 # TODO: Add mariadb-client-10.1 to Apache container
-docker exec $APACHE_CONTAINER_NAME bash -c \
-  "mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_USER_PASSWORD \
-  $DATABASE_NAME > /var/www/html/w/db.sql"
+ssh -p 2222 dserver@localhost "sudo -S docker exec $APACHE_CONTAINER_NAME bash -c \
+  'mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_USER_PASSWORD \
+  $DATABASE_NAME > /var/www/html/w/db.sql'"
 
 # Exclude .git
-tar --exclude ".*" \
-  -czvf $MEDIAWIKI_SUNFLOWER_DISTRIBUTION_FOLDER/install/mediawiki-root-folder.tar.gz \
-  $BASE_MEDIAWIKI_ROOT_FOLDER
+ssh -p 2222 dserver@localhost "tar --exclude '.*' \
+  -czvf $MEDIAWIKI_CANASTA_DISTRIBUTION_FOLDER/mediawiki-root-w-folder.tar.gz \
+  $BASE_MEDIAWIKI_ROOT_FOLDER"
+
+scp -P 2222 dserver@localhost:/home/dserver/mediawiki-root-w-folder.tar.gz .
