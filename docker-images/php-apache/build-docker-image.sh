302128cb7e948090959ddffd40c2605cda71f7cc
Lex <lex@dataspects.com>
2021-03-03 15:14:30 +0100

    incsamo

--- docker-images/php-apache/build-docker-image.sh
+++ docker-images/php-apache/build-docker-image.sh
@@ -1,8 +1,13 @@
-VERSION=2103030905f
+VERSION=2103031212incsamo
 
-cp -r ../../../mediawiki-manager/mediawiki_root/w w
+echo "Remove existing w..."
+sudo rm -rf w
 
-sudo docker build -t dataspects/php-apache:7.4.7_$VERSION .
+echo "Copy MWMSafeMode wiki..."
+sudo cp -r ../../../mediawiki-manager/mediawiki_root/w w
+
+echo "Building Docker image..."
+sudo docker build -t dataspects/php-apache:7.4.7_${VERSION} .
 
 # sudo docker login
-# sudo docker push dataspects/php-apache:7.4.7_$VERSION
+# sudo docker push dataspects/php-apache:7.4.7_${VERSION}
