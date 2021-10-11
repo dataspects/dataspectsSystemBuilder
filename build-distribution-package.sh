29c3b6c8d27a66ae59828e287aa3fa00a8cc2370
Lex Sulzer <lex@dataspects.com>
2021-10-11 12:03:46 +0200

    Pushed new dataspects/mediawiki:1.36.2-3.2.3

--- build-distribution-package.sh
+++ build-distribution-package.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-MEDIAWIKI_ROOT_FOLDER={{ system_root }}/w
-MEDIAWIKI_CANASTA_DISTRIBUTION_FOLDER=/home/dserver
-MEDIAWIKI_CANASTA_ARCHIVE=mediawiki-root-w-folder-1.35.1-3.2.2.tar.gz
+# RUN THIS MANUALLY ON dserver@dserver:~$
+
+MEDIAWIKI_ROOT_FOLDER=/home/dserver/system_root/w
+MEDIAWIKI_DISTRIBUTION_FOLDER=/home/dserver
+MEDIAWIKI_ARCHIVE=dataspects-mediawiki-root-w-folder-1.36.2-3.2.3.tar.gz
 MEDIAWIKI_DATABASE_DUMP=db.sql
 
-APACHE_CONTAINER_NAME=mediawiki_canasta
+APACHE_CONTAINER_NAME=mediawiki
 
-MYSQL_HOST=127.0.0.1
+MYSQL_HOST=mariadb
 DATABASE_NAME=mediawiki
 MYSQL_USER=mediawiki
 MYSQL_USER_PASSWORD=mediawikipass
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# TODO: ADD $wgSiteNotice = '================ MWM Safe Mode ================';
-
 # Dump database
-# TODO: Add mariadb-client-10.1 to Apache container
-ssh -p 2222 dserver@localhost "sudo -S docker exec $APACHE_CONTAINER_NAME bash -c \
-  'mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_USER_PASSWORD \
-  $DATABASE_NAME > /var/www/html/w/$MEDIAWIKI_DATABASE_DUMP'"
+docker exec $APACHE_CONTAINER_NAME bash -c \
+  "mysqldump -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_USER_PASSWORD \
+  $DATABASE_NAME > /var/www/html/w/$MEDIAWIKI_DATABASE_DUMP"
 
 # Exclude .git
-ssh -p 2222 dserver@localhost "cd $MEDIAWIKI_ROOT_FOLDER && tar --exclude '.*' \
-  -czvf $MEDIAWIKI_CANASTA_DISTRIBUTION_FOLDER/$MEDIAWIKI_CANASTA_ARCHIVE *"
+cd $MEDIAWIKI_ROOT_FOLDER && tar --exclude '.*' \
+  -czvf $MEDIAWIKI_DISTRIBUTION_FOLDER/$MEDIAWIKI_ARCHIVE *
 
 # Download
-scp -P 2222 dserver@localhost:/home/dserver/$MEDIAWIKI_CANASTA_ARCHIVE .
+# scp -P 2222 dserver@localhost:/home/dserver/$MEDIAWIKI_ARCHIVE .
 
 # Upload
 # scp -P 2222 docker-compose.yml \
-#   MEDIAWIKI_CANASTA_ARCHIVE \
+#   MEDIAWIKI_ARCHIVE \
 #   install-distribution-package.sh \
 #   dserver@localhost:/home/dserver/
--- docker-images/mediawiki/build-docker-image.sh
+++ docker-images/mediawiki/build-docker-image.sh
@@ -1,29 +1,27 @@
 IMAGENAME=mediawiki
 
 packages=(
-    "mediawiki-root-w-folder-1.35.0-3.2.1.tar.gz"
-    "mediawiki-root-w-folder-1.35.1-3.2.2.tar.gz"
+    "dataspects-mediawiki-root-w-folder-1.36.2-3.2.3.tar.gz"
 )
 
 # Rename ./versions folders first!!!
 versions=(
-    "1.35.0-2104141705"    
-    "1.35.1-2104141705"
+    "1.36.2-3.2.3"
 )
 
 # rm -rf versions/*
 for p in ${!packages[@]}
 do
-    # mkdir --parents versions/${versions[$p]}
-    # tar -xzf ${packages[$p]} -C versions/${versions[$p]}
+    mkdir --parents versions/${versions[$p]}
+    tar -xzf ${packages[$p]} -C versions/${versions[$p]}
     echo "Building Docker image $IMAGENAME:${versions[$p]}"
-    sudo docker build \
+    docker build \
         -t dataspects/$IMAGENAME:${versions[$p]} \
         --build-arg CURRENTW=versions/${versions[$p]} \
         .
     echo "Pushing Docker image $IMAGENAME:${versions[$p]}"
-    sudo docker login
-    sudo docker push dataspects/$IMAGENAME:${versions[$p]}
+    docker login
+    docker push dataspects/$IMAGENAME:${versions[$p]}
 done
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
