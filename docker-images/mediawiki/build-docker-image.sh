de3a934dd27a3928269fe736ac9ac08cc4a8cb60
Lex Sulzer <lex@dataspects.com>
2021-10-11 19:07:31 +0200

    Docker build renaming

--- docker-images/mediawiki/build-docker-image.sh
+++ docker-images/mediawiki/build-docker-image.sh
@@ -1,4 +1,5 @@
 IMAGENAME=mediawiki
+VERSION=211011
 
 packages=(
     "dataspects-mediawiki-root-w-folder-1.36.2-3.2.3.tar.gz"
@@ -16,21 +17,21 @@ for p in ${!packages[@]}
 do
     # mkdir --parents versions/${versions[$p]}
     # tar -xzf ${packages[$p]} -C versions/${versions[$p]}
-    echo "Building Docker image $IMAGENAME:${versions[$p]}-$ARCH"
+    echo "Building Docker image $IMAGENAME:${versions[$p]}_$VERSION-$ARCH"
     docker build \
-        -t dataspects/$IMAGENAME:${versions[$p]}-$ARCH \
+        -t dataspects/$IMAGENAME:${versions[$p]}_$VERSION-$ARCH \
         --build-arg CURRENTW=versions/${versions[$p]} \
         .
-    echo "Pushing Docker image $IMAGENAME:${versions[$p]}-$ARCH"
+    echo "Pushing Docker image $IMAGENAME:${versions[$p]}_$VERSION-$ARCH"
     docker login
-    docker push dataspects/$IMAGENAME:${versions[$p]}-$ARCH
+    docker push dataspects/$IMAGENAME:${versions[$p]}_$VERSION-$ARCH
 done
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # # MWStake MediaWiki Manager
 # $mwmls = "../mwmLocalSettings.php";
 # if(file_exists($mwmls)) {
-# 	require_once($mwmls);
+#       require_once($mwmls);
 # } else { 
-# 	echo "ERROR: ../mwmLocalSettings.php include not loaded.";
+#       echo "ERROR: ../mwmLocalSettings.php include not loaded.";
 # }
--- docker-images/php-apache/build-php-apache-docker-image.sh
+++ docker-images/php-apache/build-php-apache-docker-image.sh
@@ -3,8 +3,8 @@ IMAGENAME=php-apache:7.4.16_211011
 ARCH=amd64
 
 echo "Building Docker image $IMAGENAME-$ARCH"
-sudo docker build -t dataspects/$IMAGENAME-$ARCH .
+docker build -t dataspects/$IMAGENAME-$ARCH .
 
 echo "Pushing Docker image $IMAGENAME-$ARCH"
-sudo docker login
-sudo docker push dataspects/$IMAGENAME-$ARCH
+docker login
+docker push dataspects/$IMAGENAME-$ARCH
