679b2e7aabf2bdafada8b050aebdc5dbefa48f38
Lex Sulzer <lex@dataspects.com>
2021-11-01 16:12:43 +0100

    Corrected permissions

--- docker-images/mediawiki/build-docker-image.sh
+++ docker-images/mediawiki/build-docker-image.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
 IMAGENAME=mediawiki
-VERSION=211101
-
-packages=(
-    "dataspects-mediawiki-root-w-folder-1.36.2-3.2.3.tar.gz"
-)
+VERSION=2111011533
 
 # Rename ./versions folders first!!!
-versions=(
-    "1.36.2-3.2.3"
-)
+smwversion="1.36.2-3.2.3"
 
 ARCH=`arch`
 
 # rm -rf versions/*
-for p in ${!packages[@]}
-do
-    # mkdir --parents versions/${versions[$p]}
-    # tar -xzf ${packages[$p]} -C versions/${versions[$p]}
-    cat require_customizations.sh >> ./versions/${versions[$p]}/LocalSettings.php
-    echo "Appended require_customizations.sh to ./versions/${versions[$p]}/LocalSettings.php"
-    echo "{}" > ./versions/${versions[$p]}/composer.local.json
-    echo "{}" > ./versions/${versions[$p]}/composer.local.lock
-    echo "Building Docker image $IMAGENAME:${versions[$p]}_$VERSION-$ARCH"
-    docker build \
-        -t dataspects/$IMAGENAME:${versions[$p]}_$VERSION-$ARCH \
-        --build-arg CURRENTW=./versions/${versions[$p]} \
-        --build-arg ARCH=$ARCH \
-        .
-    echo "Pushing Docker image $IMAGENAME:${versions[$p]}_$VERSION-$ARCH"
-    docker login
-    docker push dataspects/$IMAGENAME:${versions[$p]}_$VERSION-$ARCH
-done
+# mkdir --parents versions/$smwversion
+# tar -xzf ${packages[$p]} -C versions/$smwversion
+
+cat require_customizations.sh >> ./versions/$smwversion/LocalSettings.php
+echo "Appended require_customizations.sh to ./versions/$smwversion/LocalSettings.php"
+echo "{}" > ./versions/$smwversion/composer.local.json
+echo "{}" > ./versions/$smwversion/composer.local.lock
+echo "Building Docker image $IMAGENAME:${smwversion}_$VERSION-$ARCH"
+docker build \
+    -t dataspects/$IMAGENAME:${smwversion}_$VERSION-$ARCH \
+    --build-arg CURRENTW=./versions/$smwversion \
+    --build-arg ARCH=$ARCH \
+    .
+echo "Pushing Docker image $IMAGENAME:${smwversion}_$VERSION-$ARCH"
+docker login
+docker push dataspects/$IMAGENAME:${smwversion}_$VERSION-$ARCH
